d35aa6ccaab2dcb6aa0d3d40e4bc8145d6a9f201
Julian Hubatsch <j.hubatsch@boewa.de>
2022-10-26 23:51:43 +0200

    refactor character card to redirect to details page

--- src/features/character/components/CharacterCard.tsx
+++ src/features/character/components/CharacterCard.tsx
@@ -1,4 +1,5 @@
 import Image from "next/future/image";
+import Link from "next/link";
 import { Character } from "../types";
 
 interface Props {
@@ -9,17 +10,23 @@ export const CharacterCard = ({ character }: Props) => {
   const { name, image: imageSrc } = character;
 
   return (
-    <div className="relative overflow-clip rounded-xl border shadow-md">
-      <Image
-        src={imageSrc}
-        alt={`Image of ${name}`}
-        width={300}
-        height={300}
-        className="duration-20 w-full transition-all hover:scale-125"
-      />
-      <div className="absolute bottom-0 flex w-full justify-center bg-black/80 p-4 backdrop-blur-sm ">
-        <span className=" truncate text-xl font-bold text-white">{name}</span>
-      </div>
-    </div>
+    <Link href={`/characters/${character.id}`} passHref>
+      <a>
+        <div className="relative overflow-clip rounded-xl border shadow-md">
+          <Image
+            src={imageSrc}
+            alt={`Image of ${name}`}
+            width={300}
+            height={300}
+            className="duration-20 w-full transition-all hover:scale-125"
+          />
+          <div className="absolute bottom-0 flex w-full justify-center bg-black/80 p-4 backdrop-blur-sm ">
+            <span className=" truncate text-xl font-bold text-white">
+              {name}
+            </span>
+          </div>
+        </div>
+      </a>
+    </Link>
   );
 };
