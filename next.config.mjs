70f5acca9c65927589f9549274be9aecea1afbdd
Julian Hubatsch <j.hubatsch@boewa.de>
2022-10-26 16:55:47 +0200

    cleanup unused files

--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,6 @@
 !process.env.SKIP_ENV_VALIDATION && (await import("./src/env/server.mjs"));
 
 /**
- * Don't be scared of the generics here.
- * All they do is to give us autocompletion when using this.
- *
  * @template {import('next').NextConfig} T
  * @param {T} config - A generic parameter that flows through to the return type
  * @constraint {{import('next').NextConfig}}
@@ -15,15 +12,13 @@ function defineNextConfig(config) {
 }
 
 export default defineNextConfig({
+  redirects: async () => [
+    {
+      source: "/",
+      destination: "/characters",
+      permanent: true,
+    },
+  ],
   reactStrictMode: true,
   swcMinify: true,
-  /** Next.js i18n docs:
-   * @see https://nextjs.org/docs/advanced-features/i18n-routing
-   * Reference repo for i18n:
-   * @see https://github.com/juliusmarminge/t3-i18n
-   **/
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
 });
---
+++ src/pages/characters/index.tsx
@@ -0,0 +1,5 @@
+const CharactersPage = () => {
+  return <div>Chars</div>;
+};
+
+export default CharactersPage;
