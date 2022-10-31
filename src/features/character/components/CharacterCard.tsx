9af71783496b61840e0692677c61224a8859ac78
Julian Hubatsch <j.hubatsch@boewa.de>
2022-10-31 03:18:30 +0100

    match character card styling with other parts of the app

--- src/features/character/components/CharacterCard.tsx
+++ src/features/character/components/CharacterCard.tsx
@@ -11,24 +11,31 @@ export const CharacterCard = ({ character }: Props) => {
   const { name, image: imageSrc } = character;
 
   return (
-    <div className="relative w-[300px] overflow-clip rounded-2xl bg-black/60">
+    <div className="group relative w-[300px] overflow-hidden rounded-2xl bg-black/40 p-2 transition-all after:absolute after:inset-0 after:-z-[1] after:-translate-x-full after:rounded-2xl after:bg-gradient-to-r after:from-violet-600 after:to-indigo-600 after:opacity-0 after:transition-all after:duration-300  hover:bg-transparent hover:after:translate-x-0 hover:after:opacity-100">
       <Link href={`/characters/${character.id}`} passHref>
-        <a>
-          <Image
-            src={imageSrc}
-            alt={`Image of ${name}`}
-            width={300}
-            height={300}
-            className="duration-20 w-full transition-all hover:scale-125"
-          />
-          <div className="absolute bottom-0 flex w-full justify-center bg-black/80 p-4 backdrop-blur-sm">
-            <span className=" truncate text-xl font-bold text-white">
+        {/* Clip to keep rounded corners */}
+        <a className="relative block overflow-clip rounded-2xl">
+          {/* Clip the image scaling */}
+          <div className="overflow-clip">
+            <Image
+              src={imageSrc}
+              alt={`Image of ${name}`}
+              width={300}
+              height={300}
+              className="transition-all duration-300 group-hover:scale-110"
+            />
+          </div>
+          <div className="flex w-full justify-center bg-black/40 p-4">
+            <span
+              className="truncate text-xl font-bold text-white"
+              title={name}
+            >
               {name}
             </span>
           </div>
         </a>
       </Link>
-      <div className="group-hover: absolute top-0 right-0 rounded-bl-2xl bg-black/80 p-2 backdrop-blur-sm">
+      <div className="absolute top-0 right-0 rounded-bl-2xl bg-black/80 p-2 backdrop-blur-sm">
         <LazyFavoriteMark character={character} />
       </div>
     </div>
