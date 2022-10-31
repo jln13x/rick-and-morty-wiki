c041a0a69664eae702e766e804189d3ee4f9ddd3
Julian Hubatsch <j.hubatsch@boewa.de>
2022-10-31 03:17:50 +0100

    more colorful galaxy background

--- src/features/galaxy/GalaxyBackground.tsx
+++ src/features/galaxy/GalaxyBackground.tsx
@@ -1,9 +1,9 @@
 export const GalaxyBackground = () => {
   return (
     <>
-      <div className="absolute inset-0 z-[7] bg-gradient-to-r from-violet-900 to-black"></div>
-      <div className="absolute inset-0 z-[8] bg-gradient-to-bl from-indigo-600 to-black opacity-40"></div>
-      <div className="absolute inset-0 z-10 bg-black/40"></div>
+      <div className="absolute inset-0 z-[7] bg-gradient-to-br from-violet-900 via-sky-900  to-indigo-900"></div>
+      <div className="absolute inset-0 z-[8] bg-gradient-to-bl from-indigo-600 via-violet-600 to-sky-600 opacity-40"></div>
+      <div className="absolute inset-0 z-10 bg-black/50"></div>
     </>
   );
 };
